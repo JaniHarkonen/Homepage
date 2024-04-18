2e3ad33c1344fa820cf1bf650b608b9d1618e082
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-04-18 17:12:21 +0300

    Added controls to the project carousel.

--- src/app/components/project-carousel/project-carousel.component.ts
+++ src/app/components/project-carousel/project-carousel.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TimelineComponent } from '../timeline/timeline.component';
+import { nmod } from '../../../utils';
 
 @Component({
   selector: 'app-project-carousel',
@@ -9,5 +10,22 @@ import { TimelineComponent } from '../timeline/timeline.component';
   styleUrl: './project-carousel.component.css'
 })
 export class ProjectCarouselComponent {
+  public selectedProjectIndex: number = 0;
+  public selectedProject: any = null;
 
+  @Input() projects: any;
+  @Input() projectOrder: number[] = [];
+
+  ngOnInit() {
+    this.selectProject(0);
+  }
+
+  public advanceCarousel(advance: number): void {
+    this.selectProject(this.selectedProjectIndex + advance);
+  }
+
+  public selectProject(index: number): void {
+    this.selectedProjectIndex = nmod(index, this.projectOrder.length);
+    this.selectedProject = this.projects[this.projectOrder[this.selectedProjectIndex]];
+  }
 }
--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.css'
 })
 export class TimelineComponent {
+  @Input() indices: number[] = [];
+  @Input() selectedIndex: number = 0;
+  @Output() selectElement: EventEmitter<number> = new EventEmitter<number>();
 
+  public onSelect(index: number): void {
+    this.selectElement.emit(index);
+  }
+
+  public onAdvance(advance: number): void {
+    this.selectElement.emit(this.selectedIndex + advance);
+  }
 }
