95e46a357d700b85180fbb01c51f7decf976e374
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-04-19 20:07:08 +0300

    Added logic to project carousel, finished styling, test data.

--- src/app/components/project-carousel/project-carousel.component.ts
+++ src/app/components/project-carousel/project-carousel.component.ts
@@ -1,23 +1,63 @@
 import { Component, Input } from '@angular/core';
 import { TimelineComponent } from '../timeline/timeline.component';
 import { nmod } from '../../../utils';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProjectInfo } from '../../../assets/projects/projects';
+import { CONFIG } from '../../../pathConfig';
+
+/**
+ * Available description verbosities. Currently only brief and full
+ * verbosities exist, though this is unlikely to change in the future.
+ * 
+ * `BRIEF` short, brief, description to get the gist of the project
+ * 
+ * `FULL` a full description of the project outlining some of the 
+ * key techniques and technologies involved in developing it
+ */
+enum DescriptionVerbosity {
+  BRIEF,
+  FULL
+}
 
 @Component({
   selector: 'app-project-carousel',
   standalone: true,
-  imports: [TimelineComponent],
+  imports: [TimelineComponent, HttpClientModule],
   templateUrl: './project-carousel.component.html',
   styleUrl: './project-carousel.component.css'
 })
 export class ProjectCarouselComponent {
+  public descriptionVerbosity: DescriptionVerbosity = DescriptionVerbosity.BRIEF;
   public selectedProjectIndex: number = 0;
-  public selectedProject: any = null;
+  public selectedProject: any = {
+    info: null,
+    briefHTML: "",
+    descriptionHTML: ""
+  };
+
+  private httpClient: HttpClient;
 
   @Input() projects: any;
   @Input() projectOrder: number[] = [];
 
-  ngOnInit() {
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  ngOnInit(): void {
     this.selectProject(0);
+  }
+
+  private fetchDescriptions(projectInfo: ProjectInfo): void {
+    this.httpClient.get(
+      CONFIG.paths.projects + projectInfo.briefURL, 
+      { responseType: "text" }
+    ).subscribe((data) => this.selectedProject.briefHTML = data);
+
+    this.httpClient.get(
+      CONFIG.paths.projects + projectInfo.descriptionURL,
+      { responseType: "text" }
+    ).subscribe((data) => this.selectedProject.descriptionHTML = data)
   }
 
   public advanceCarousel(advance: number): void {
@@ -26,6 +66,40 @@ export class ProjectCarouselComponent {
 
   public selectProject(index: number): void {
     this.selectedProjectIndex = nmod(index, this.projectOrder.length);
-    this.selectedProject = this.projects[this.projectOrder[this.selectedProjectIndex]];
+    this.selectedProject = {
+      ...this.selectedProject,
+      info: this.projects[this.projectOrder[this.selectedProjectIndex]] 
+    }
+
+    this.fetchDescriptions(this.selectedProject.info);
+    this.descriptionVerbosity = DescriptionVerbosity.BRIEF;
+  }
+
+  public getVideoURL(): string {
+    return CONFIG.paths.projects + this.selectedProject.info.videoURL;
+  }
+
+
+    // Lots of redundancy here for now, clean this up
+
+  public toggleDescription(): void {
+    if( this.descriptionVerbosity === DescriptionVerbosity.BRIEF )
+    this.descriptionVerbosity = DescriptionVerbosity.FULL; 
+    else
+    this.descriptionVerbosity = DescriptionVerbosity.BRIEF;
+  }
+
+  public getDescriptionHTML(): string {
+    if( this.descriptionVerbosity === DescriptionVerbosity.BRIEF )
+    return this.selectedProject.briefHTML;
+    else
+    return this.selectedProject.descriptionHTML;
+  }
+
+  public getDescriptionButtonCaption(): string {
+    if( this.descriptionVerbosity === DescriptionVerbosity.BRIEF )
+    return "Read more";
+    else
+    return "Less";
   }
 }
--- src/assets/projects/projects.ts
+++ src/assets/projects/projects.ts
@@ -1,25 +1,59 @@
 export type ProjectStatus = {
   readonly title: string
-};
+}
 
 export type ProjectInfo = {
   readonly index: number,
   readonly name: string,
-  readonly description: string
+  readonly briefURL: string,
+  readonly descriptionURL: string,
+  readonly videoURL: string
   //readonly status: ProjectStatus
 }
 
-const createProject = (index: number, name: string, description: string): ProjectInfo => {
+const createProject = (
+  index: number, 
+  name: string, 
+  briefURL: string, 
+  descriptionURL: string,
+  videoURL: string
+): ProjectInfo => {
   return {
     index,
     name,
-    description
+    briefURL,
+    descriptionURL,
+    videoURL
   };
 }
 
 export const PROJECTS = {
-  0: createProject(0, "JOHNEngine", "A multi-threaded 3D Java game engine built using LWJGL and OpenGL."),
-  1: createProject(1, "Editor2D2", "Generic 2D-level editor that can be used to create levels for tile-based 2D games. Due to its modular nature it can easily be extended and adapted to the needs of any particular 2D-game project."),
-  2: createProject(2, "DumpEmSuite", "Electron-based investment tool I built to aid myself with filtering and analysing potential investment candidates. An SQL-based database is used to store the company specific stock data."),
-  3: createProject(3, "MerchBetter", "One of my earlier Electron applications. A utilitiy tool that helps you keep notes of which items you're flipping in RuneScape.")
+  0: createProject(
+    0, 
+    "JOHNEngine", 
+    "johnengine/brief.html", 
+    "johnengine/desc.html",
+    "testing/test.mp4"
+  ),
+  1: createProject(
+    1, 
+    "Editor2D2", 
+    "editor2d2/brief.html", 
+    "editor2d2/desc.html",
+    "testing/test2.mp4"
+  ),
+  2: createProject(
+    2, 
+    "DumpEmSuite", 
+    "dumpem-suite/brief.html", 
+    "dumpem-suite/desc.html",
+    "testing/test.mp4"
+  ),
+  3: createProject(
+    3, 
+    "MerchBetter", 
+    "merch-better/brief.html", 
+    "merch-better/desc.html",
+    "testing/test2.mp4"
+  )
 }
