466714ea63ccff85d947d07bb88af689ae012e55
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-05-01 22:27:30 +0300

    Added auto-transfer to the landing page.

--- src/app/animations/expand-collapse.anim.ts
+++ src/app/animations/expand-collapse.anim.ts
@@ -2,10 +2,8 @@ import { AnimationTriggerMetadata, animate, state, style, transition, trigger } 
 
 export const ExpandCollapseAnimation: AnimationTriggerMetadata = trigger("expandCollapse", [
   state("expanded", style({ maxHeight: "100%" })),
-  /*state("collapsed", style({ maxHeight: "30%", overflowY: "auto" })),*/
-  /*transition("expanded => collapsed", [animate("1s ease-in")]),*/
   transition("* => expanded", [
     animate("0s", style({ overflowY: "hidden" })),
-    animate("1s ease-out")
+    animate("500ms ease-out")
   ])
 ]);
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TechnologyStackComponent } from '../../components/technology-stack/technology-stack.component';
 import { TECHNOLOGIES } from '../../../assets/technologies/technologies';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [TechnologyStackComponent],
+  imports: [TechnologyStackComponent, RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
@@ -13,6 +14,19 @@ export class LandingComponent {
   private usedTechnologies: string [] = [
     TECHNOLOGIES["angular"].id
   ];
+
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+  }
+
+  ngOnInit() {
+    setTimeout(
+      () => this.router.navigateByUrl("/about"), 
+      8000
+    );
+  }
 
   public getTechnologies(): string[] {
     return this.usedTechnologies;
