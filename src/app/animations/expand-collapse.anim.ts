bbeab3014e8a4feff6416c6e4993af4357fe9837
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-05-01 15:33:40 +0300

    Fixed scrolling animations in project carousel.

--- src/app/animations/expand-collapse.anim.ts
+++ src/app/animations/expand-collapse.anim.ts
@@ -2,7 +2,10 @@ import { AnimationTriggerMetadata, animate, state, style, transition, trigger } 
 
 export const ExpandCollapseAnimation: AnimationTriggerMetadata = trigger("expandCollapse", [
   state("expanded", style({ maxHeight: "100%" })),
-  state("collapsed", style({ maxHeight: "30%" })),
-  transition("expanded => collapsed", [animate("1s ease-in")]),
-  transition("collapsed => expanded", [animate("1s ease-out")])
+  /*state("collapsed", style({ maxHeight: "30%", overflowY: "auto" })),*/
+  /*transition("expanded => collapsed", [animate("1s ease-in")]),*/
+  transition("* => expanded", [
+    animate("0s", style({ overflowY: "hidden" })),
+    animate("1s ease-out")
+  ])
 ]);
